30cb1edea860e7830ad1734a6c13553fddd7f282
Diego C. Bertolucci <bertolucci.diego.c@gmail.com>
2024-01-28 20:07:11 -0300

    # Reception of client data via request body

--- src/main/java/com/example/SpringEx10/controllers/PaymentController.java
+++ src/main/java/com/example/SpringEx10/controllers/PaymentController.java
@@ -5,19 +5,19 @@ import com.example.SpringEx10.services.PaymentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @RestController
 public class PaymentController {
-    private final PaymentService paymentService;
 
-    public PaymentController(PaymentService paymentService) {
-        this.paymentService = paymentService;
-    }
+    private Logger logger = Logger.getLogger(PaymentController.class.getName());
 
     @PostMapping("/payment")
-    public ResponseEntity<?> makePayment() {
-        PaymentDetails paymentDetails = paymentService.processPayment();
+    public ResponseEntity<?> makePayment(@RequestBody PaymentDetails paymentDetails) {
+        logger.info("Received payment " + paymentDetails.getAmount());
 
         return ResponseEntity
                 .status(HttpStatus.ACCEPTED)
